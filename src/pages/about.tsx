4f19d0d83ddf3f61c4ff31b2d9a81c3dd2ecae88
Madekuji <27665110+Madekuji@users.noreply.github.com>
2025-05-20 02:26:39 +0800

    adjusted work card hover and made about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,73 @@
 function About(){
     return(
-        <div>
-            <p>This is the about page!</p>
+        <div className="aboutPage">
+            <div className="aboutBox">
+                <h2>
+                    “I’m Madekuji, your resident virtual tech support and subtitler! I want to be an otaku for others!”
+                    </h2>
+                <p>
+                    Hi hello! My name is Madekuji Masamune. I also go by the username Madekuji-san, but you can call me MaDe! (pronounced マデ!)
+                </p>
+                <p>
+                    I'm a huge otaku! For a lot of things!!! I love anime, manga (especially doujinshi), video games, music, cosplay, tech, trains, fanfics, and VTubers!
+                </p>
+                <p>
+                    I also do all sorts of work, like:<br />
+                    Web development, UI/UX design, systems administration, tech support, fansubbing, voice acting, event organizing, and more!
+                </p>
+                <p>
+                    …and I'm now also a VTuber myself, I guess. It wasn't always like that though!
+                </p>
+                <p>
+                    I've been making YouTube videos since I was a kid and started doing live streams in high school. I've always shared my voice if it meant sharing the things I like, or if it meant helping people learn something new.
+                </p>
+                <p>
+                    Since then up to now, I always want to keep making things I like; not just for myself, but in the service of others! Whenever people become happy from the things I do, it makes me happy too!
+                </p>
+                <p>
+                    I’m first and foremost a fan, and a huge chunk of what I do comes from the inspiration given to me by the things I like. It's from them I source my aspirations and creativity.
+                </p>
+                <hr />
+                <h3>
+                    What do you work on and where do you work for?
+                </h3>
+                <p>
+                    I stream on Twitch! I also make VTuber clips sometimes over at Masao T.
+                </p>
+                <p>
+                    I currently work (in a volunteer capacity) as:
+                    <ul>
+                        <li>Co-manager and Team Lead for Subtitles at HoloResort Translations (formerly Hololive Resort Subs)</li>
+                        <li>UI/UX Designer at Holodex</li>
+                        <li>Field Reporter and Manila TOC Broadcast Director at VTuber NewsDrop</li>
+                        <li>Cast Member (Event Staff) at Moomin’ Manila</li>
+                        <li>Head of Promotions, Production Member, Technical Staff, and Brand Designer at HoloPro Fans PH</li>
+                    </ul>
+                </p>
+                <p>
+                    I was formerly:
+                    <ul>
+                        <li>Staff Member at Touyou no Shinju (Toushin)</li>
+                        <li>Discord Moderator at KoMETA Fan Server</li>
+                    </ul>
+                </p>
+                <p>
+                    If you would like to hire me or commission me for any projects, do send an email to hewwo@mdkj.dev!
+                </p>
+                <hr />
+                <h3>
+                    Where did the name “Madekuji” come from?
+                </h3>
+                <p>
+                    Yeah, yeah, I know. It's not a real Japanese name. If anything, it’s more like fake or pseudo-Japanese. That's why it’s spelled in Katakana! It’s no secret that I was a weeb back when I was young (and I still have some tendencies!), but I do still keep it around because of what my name means to me now.
+                </p>
+                <p>
+                    I think my name is a byproduct of the journey of understanding a culture different from the one you started with; one that is unique, fascinating, and beautiful. It's like making your first crayon drawing. Sure, it’s no Mona Lisa, and it's a bit embarrassing looking at it as you grow older, but it's a reminder of where you started when you didn't know any better and a reflection of the passion you've always had and still carry along with you.
+                </p>
+                <p>
+                    If the name’s a bit long, you can simply call me MaDe! I got that name from friends who were a bit tired saying the full thing after a while… 😅
+                </p>
+            </div>
         </div>
     );
     }
